a1080741198bf10fe6eb39ccce817a5b3b40d83f
Zyrek911 <zyrekxx666xx@gmail.com>
2025-09-03 04:12:40 +0000

    D: created a basic game loop

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from asteroids!")
+import pygame
+from constants import *
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    i = 0
+    while(i == 0):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
